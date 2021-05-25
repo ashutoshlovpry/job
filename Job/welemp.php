199a02d85d43e8677b3c3b09bab39394c945774f
ashutoshlovpry <67589741+ashutoshlovpry@users.noreply.github.com>
2021-05-25 16:50:51 +0530

    Update welemp.php

--- Job/welemp.php
+++ Job/welemp.php
@@ -35,8 +35,8 @@ if(isset($_SESSION['log']))
     <body>
         <nav>
              <div class="nav-wrapper">
-                 <a href="#" class="brand-logo" style="margin-left: 750px"> SeekJob</a>
-                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                 
+                 
                  <ul id="nav-mobile" class="right hide-on-med-and-down">
                      <li><a href="signout.php">Logout</a></li>
                      <li><a href="emp_prof.php">Profile</a></li>
